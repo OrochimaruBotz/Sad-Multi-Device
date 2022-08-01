6f40e5107d9eb64cd0f974b339ded957b0189922
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-08-02 05:59:13 +0700

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -2,15 +2,15 @@ let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
 *💌 Nama* : Yamato
-*✉️ Nama RL* : Iqbal 
+*✉️ Nama RL* : Manusia
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 20 Mei 2003
+*⏰ Tanggal lahir* : Pikir Sendiri
 *🎨 Umur* : Pikir Sendiri
 *🧮 Kelas* : Pikir Sendiri
 *🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
 *💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Riau, Indragiri Hulu, Air Molek
+*🗺️ Tinggal* : Indo, JawaTimur, Surabaya
 *❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
 *💔 Benci* : autis, anak epep, seleb
 
