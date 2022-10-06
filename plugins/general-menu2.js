d4850ef4694f284965aa726ef5cf7acfdecee8f9
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-10-07 05:55:33 +0700

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -4,7 +4,7 @@ let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
 conn.sendHydrated(m.chat, '*『 GojoBot 』*', `ʜᴀɪ,ᴀᴋᴜ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ʏᴀɴɢ ᴅɪʙᴇʀɪ ɴᴀᴍᴀ GojoBot,ᴀᴋᴜ ᴅɪ ᴋᴇᴍʙᴀɴɢᴋᴀɴ ᴏʟᴇʜ ꜱᴇꜱᴇᴏʀᴀɴɢ ʏᴀɴɢ ʙᴇʀɴᴀᴍᴀ Top global ngesamtir,ʙᴇʟɪᴀᴜ ᴍᴀꜱɪʜ ᴘᴇᴍᴜʟᴀ ᴅᴀɴ ᴍᴀꜱɪʜ ʙᴇʟᴀᴊᴀʀ ʙᴀɴʏᴀᴋ ʜᴀʟ,ᴅᴀɴ ᴛᴜᴊᴜᴀɴɴʏᴀ ʜᴀɴʏᴀ ɪɴɢɪɴ ᴛᴀᴜ ᴀᴘᴀ ʏᴀɴɢ ɪɴɢɪɴ ᴅɪᴋᴇᴛᴀʜᴜɪ ᴅɪ ᴅᴜɴɪᴀ ʙᴏᴛ,\nꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/menu.jpg', '+62 888-0494-7282', 'Nomer Owner', null, null, [
 ['Menu`, '.m'],
-['Owner`, '.owner']
+['Donasi`, '.donasi']
 ], m, {asLocation: true})
 }
 handler.help = ['menu']
